7d30a73fa2fc85576f8649342db3614fd9ff5b03
sathish-avunoori <sathish.avunoori@kofax.com>
2016-05-19 12:18:55 +0530

    update
    update

--- TestApp/processqueue/src/test/java/com/kofax/processqueue/test/test/BaseQueueUnitTest.java
+++ TestApp/processqueue/src/test/java/com/kofax/processqueue/test/test/BaseQueueUnitTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@RunWith(AndroidJUnit4.class)
+
 public class BaseQueueUnitTest {
     BaseQueue<QueueModel> baseQueue;
 
